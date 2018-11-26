5a3accbf87c387c42c577a733f1507739bf7767f
Vitaliy Marushko <v.marushko@ossystem.com.ua>
2018-11-26 14:39:51 +0200

    Init commit

---
+++ app.ts
@@ -0,0 +1,35 @@
+import createError = require('http-errors');
+import express = require('express');
+import cookieParser = require('cookie-parser');
+import logger = require('morgan');
+
+// import indexRouter = require('./routes/index');
+// import usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err: any, req: any, res: any, next: any) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export = app;
